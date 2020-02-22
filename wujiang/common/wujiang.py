fe9ad5d1383270db30d9f874306506b4e99a23e4
jiz148 <jiz148@ucsd.edu>
2020-02-22 14:39:04 -0800

    added cards

--- wujiang/common/wujiang.py
+++ wujiang/common/wujiang.py
@@ -6,12 +6,15 @@ import json
 import os
 import random
 
+from PIL import Image, ImageDraw, ImageFont
 
 PY_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 PROJECT_PATH = os.path.dirname(PY_FILE_PATH)
 DATA_PATH = os.path.join(PROJECT_PATH, 'data')
 JSON_NAME = 'wujiang_index.json'
 JSON_PATH = os.path.join(DATA_PATH, JSON_NAME)
+IMAGE_PATH = os.path.join(DATA_PATH, 'cards')
+FONT_PATH = os.path.join(PY_FILE_PATH, 'fonts')
 
 
 class Wujiang:
@@ -33,11 +36,11 @@ class Wujiang:
         self.level = int(character.get('等级'))
         self.profession = character.get('职业')
         self.name = character.get('名称')
-        self.attack = float(character.get('攻'))
-        self.defense = float(character.get('受'))
-        self.speed = float(character.get('速'))
-        self.ranging = float(character.get('范'))
-        self.mag = float(character.get('魔'))
+        self.attack = character.get('攻')
+        self.defense = character.get('受')
+        self.speed = character.get('速')
+        self.ranging = character.get('范')
+        self.mag = character.get('魔')
         self.spells = character.get('技能')
         self.specs = character.get('性质')
         self.scepter = character.get('神杖加强')
@@ -53,3 +56,39 @@ class Wujiang:
         random_id = random.randint(0, len(key_list) - 1)
         random_name = key_list[random_id]
         self.construct(random_name)
+
+    def to_card(self, card_path=IMAGE_PATH):
+        """
+        Generates a image to image path
+        """
+        img = Image.new('RGB', (450, 650), color=(243, 210, 119))
+        draw = ImageDraw.Draw(img)
+        regular_font_path = os.path.join(FONT_PATH, 'chinese.msyh.ttf')
+
+        draw.text((10, 10), self.name, font=ImageFont.truetype(regular_font_path, 30), fill='black')
+        draw.text((10, 60), '攻: ' + self.attack, font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((100, 60), '受: ' + self.defense, font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((190, 60), '速: ' + self.speed, font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((280, 60), '范: ' + self.ranging, font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((370, 60), '魔: ' + self.mag, font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((10, 110), '技能:', font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((10, 140), self._auto_new_line(self.spells, 31), font=ImageFont.truetype(regular_font_path, 14), fill='black')
+        draw.text((10, 370), '性质:', font=ImageFont.truetype(regular_font_path, 20), fill='black')
+        draw.text((10, 400), self._auto_new_line(self.specs, 31), font=ImageFont.truetype(regular_font_path, 14), fill='black')
+        card_name = self.name + '.png'
+        card_path = os.path.join(card_path, card_name)
+
+        img.save(card_path)
+
+    def _auto_new_line(self, string, width):
+        """
+        Given a string, change auto change to string with new line
+        """
+        count = 0
+        for i in range(len(string)):
+            count += 1
+            if count > width:
+                string = string[0:i] + '\n' + string[i:]
+                i += 1
+                count = 0
+        return string
--- wujiang/main.py
+++ wujiang/main.py
@@ -2,10 +2,16 @@
 main.py
 """
 import json
+import os
 
 
 from utils.csv_converter import CsvConverter
 from common.wujiang import Wujiang
+
+PY_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(PY_FILE_PATH, 'data')
+JSON_NAME = 'wujiang_index.json'
+JSON_PATH = os.path.join(DATA_PATH, JSON_NAME)
 
 
 def dump_json():
@@ -37,5 +43,16 @@ def to_sqlite():
     csv_conveter.close()
 
 
+def print_imgs():
+    """
+    Print a image
+    """
+    with open(JSON_PATH, 'r') as fp:
+        data = json.loads(fp.read())
+    for w in data:
+        wujiang = Wujiang(w)
+        wujiang.to_card()
+
+
 if __name__ == "__main__":
-    to_sqlite()
+    print_imgs()
