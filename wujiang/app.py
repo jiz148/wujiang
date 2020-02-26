be047767b5810c445af609913b215a6d5a35bb19
jiz148 <jizjiz148148@gmail.com>
2020-02-26 12:34:30 -0800

    updated api

--- wujiang/app.py
+++ wujiang/app.py
@@ -76,6 +76,32 @@ wujiang_schema = WujiangSchema()
 wujiangs_schema = WujiangSchema(many=True)
 
 
+# create a wujiang
+@app.route('/wujiang', methods=['POST'])
+def add_wujiang():
+    attrs = _get_all_attrs()
+
+    print(attrs)
+
+    new_wujiang = Wujiang(
+        attrs[0],
+        attrs[1],
+        attrs[2],
+        attrs[3],
+        attrs[4],
+        attrs[5],
+        attrs[6],
+        attrs[7],
+        attrs[8],
+        attrs[9],
+        attrs[10])
+
+    db.session.add(new_wujiang)
+    db.session.commit()
+
+    return wujiang_schema.jsonify(new_wujiang)
+
+
 # Get Single Wujiang
 @app.route('/wujiang/<id>', methods=['GET'])
 def get_product(id):
@@ -102,6 +128,84 @@ def get_wujiangs():
             abort(400)
 
     return wujiangs_schema.jsonify(result) if result.all() else abort(404)
+
+
+# update a wujiang
+@app.route('/wujiang/<id>', methods=['PUT'])
+def update_wujiang(id):
+    updating_wujiang = Wujiang.query.get_or_404(id)
+
+    attrs = _get_all_attrs()
+
+    # Update if not None
+    updated_wujiang = _update_all_attrs(updating_wujiang, attrs)
+
+    db.session.commit()
+
+    return wujiang_schema.jsonify(updated_wujiang)
+
+
+# Delete Wujiang
+@app.route('/wujiang/<id>', methods=['DELETE'])
+def delete_product(id):
+    deleting_wujiang = Wujiang.query.get_or_404(id)
+    db.session.delete(deleting_wujiang)
+    db.session.commit()
+
+    return wujiang_schema.jsonify(deleting_wujiang)
+
+
+def _get_all_attrs():
+    """
+    Gets all the attributes of wujiang from asdf
+    @return: <Tuple> Tuple of attributes
+    """
+    name = request.json.get('name')
+    level = request.json.get('level')
+    profession = request.json.get('profession')
+    attack = request.json.get('attack')
+    defense = request.json.get('defense')
+    speed = request.json.get('speed')
+    ranging = request.json.get('ranging')
+    mag = request.json.get('mag')
+    spells = request.json.get('spells')
+    specs = request.json.get('specs')
+    scepter = request.json.get('scepter')
+
+    return name, level, profession, attack, defense, speed, ranging, mag, spells, specs, scepter
+
+
+def _update_all_attrs(wujiang, attrs):
+    """
+    Updates wujiang with tuple of new attributes only if the attribute is not None
+    @param wujiang: updating wujiang
+    @param attrs: new attrs
+    @return: updated wujiang
+    """
+    if attrs[0]:
+        wujiang.name = attrs[0]
+    if attrs[1]:
+        wujiang.level = attrs[1]
+    if attrs[2]:
+        wujiang.profession = attrs[2]
+    if attrs[3]:
+        wujiang.attack = attrs[3]
+    if attrs[4]:
+        wujiang.defense = attrs[4]
+    if attrs[5]:
+        wujiang.speed = attrs[5]
+    if attrs[6]:
+        wujiang.ranging = attrs[6]
+    if attrs[7]:
+        wujiang.mag = attrs[7]
+    if attrs[8]:
+        wujiang.spells = attrs[8]
+    if attrs[9]:
+        wujiang.specs = attrs[9]
+    if attrs[10]:
+        wujiang.scepter = attrs[10]
+
+    return wujiang
 
 
 # Run Server
